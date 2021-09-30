ad324fcadd85081dbf4cb87a364bf3aefd2d6e87
cypherz01 <mrtomcuthbertson@gmail.com>
2021-09-30 09:52:42 +0000

    read feature added

--- src/components/Read/Read.js
+++ src/components/Read/Read.js
@@ -1,11 +1,58 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, {useState, useEffect} from 'react';
+import { Button, Icon, Label, Menu, Table } from 'semantic-ui-react'
 import './Read.css';
+import axios from 'axios';
 
 export default function Read() {
+  const [tableData, setTableData] =useState([]);
+
+  function callMockApi(){
+
+  const endPointURL= "https://6151d1954a5f22001701d471.mockapi.io/people";
+
+  axios
+    .get(endPointURL)
+    .then((response) => setTableData(response.data))
+    .catch((response) => console.log(response))
+  }
+
+  function setLocalStorage(data){
+    localStorage.setItem("id", data.id)
+    localStorage.setItem("firstName", data.id)
+    localStorage.setItem("lastName", data.id)
+  }
+
+  useEffect(() => {
+    callMockApi();
+  }, [])
+
   return (
     <div>
-      Read function
-    </div>
+    <Table celled>
+    <Table.Header>
+      <Table.Row>
+        <Table.HeaderCell>ID</Table.HeaderCell>
+        <Table.HeaderCell>First Name</Table.HeaderCell>
+        <Table.HeaderCell>Last Name</Table.HeaderCell>
+      </Table.Row>
+    </Table.Header>
+    <Table.Body>
+      {tableData.map((data) => (
+      <Table.Row key = {data.id}>
+        <Table.Cell>{data.id}</Table.Cell>
+        <Table.Cell>{data.firstName}</Table.Cell>
+        <Table.Cell>{data.lastName}</Table.Cell>
+        <Table.Cell>
+          <Button color= "green" onClick={() => setLocalStorage(data)} >Update</Button>
+        </Table.Cell>
+        <Table.Cell>
+          <Button color= "red" onClick={() => setLocalStorage(data)}>Delete</Button>
+        </Table.Cell>
+
+      </Table.Row>
+      ))}
+    </Table.Body>
+  </Table>
+  </div>
   )
 }
