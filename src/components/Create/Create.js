644385017907d1d9ecb9363051bf6683a832493b
cypherz01 <mrtomcuthbertson@gmail.com>
2021-10-05 13:44:14 +0000

    added all fields(fucntionality not added)

--- src/components/Create/Create.js
+++ src/components/Create/Create.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import "./Create.css";
-import { Button, Checkbox, Form, Dropdown } from "semantic-ui-react";
+import { Button, Checkbox, Form, Dropdown, Radio } from "semantic-ui-react";
 import axios from "axios";
 import { useHistory } from 'react-router';
 
@@ -57,18 +57,16 @@ export default function Create() {
   return (
     <div>
       <Form>
-        <Form.Field required >
+
+        <Form.Group required widths="equal" >
           <label>Prefix</label>
           <Dropdown
             required
             placeholder="prefix"
-            fluid
             selection
             options={prefixOptions}
             onChange={(e, data) => setPrefix(data.value)}
           />
-        </Form.Field>
-        <Form.Group widths="equal">
           <Form.Input
             required
             fluid
@@ -84,24 +82,130 @@ export default function Create() {
             onChange={(e) => setLastName(e.target.value)}
           />
         </Form.Group>
+
+
         <Form.Field>
-          <Form.Select
-            required
-            fluid
-            label="Gender"
-            options={genderOptions}
-            placeholder="Gender"
-            onChange={(e, data) => setGender(data.value)}
-          />
-        </Form.Field>
-        <Form.Field>
-          <label>Phone Number</label>
+          <label>telephone Number</label>
           <input
             required
             placeholder="phoneNumber"
             onChange={(e) => setPhoneNumber(e.target.value)}
           />
         </Form.Field>
+
+        <Form.Group widths="equal">
+          <Form.Input
+            required
+            fluid
+            label="Address Line 1"
+            placeholder="Address Line 1"
+          />
+          <Form.Input
+            required
+            fluid
+            label="Address Line 2"
+            placeholder="Address Line 2"
+          />
+        </Form.Group>
+
+        <Form.Group widths="equal">
+          <Form.Input
+            required
+            fluid
+            label="City"
+            placeholder="City"
+          />
+          <Form.Input
+            required
+            fluid
+            label="Postcode"
+            placeholder="Postcode"
+          />
+        </Form.Group>
+
+
+        <Form.Field >
+          <label>Vehicle Type</label>
+          <Dropdown
+            required
+            placeholder="Vehicle  Type"
+            fluid
+            selection
+            options={prefixOptions}
+            onChange={(e, data) => setPrefix(data.value)}
+          />
+        </Form.Field>
+
+        <Form.Field >
+          <label>Engine Size</label>
+          <Dropdown
+            required
+            placeholder="Engine Size"
+            fluid
+            selection
+            options={prefixOptions}
+            onChange={(e, data) => setPrefix(data.value)}
+          />
+        </Form.Field>
+
+        <Form.Field >
+          <label>Additional Drivers</label>
+          <Dropdown
+            required
+            placeholder="Additional Drivers"
+            fluid
+            selection
+            options={prefixOptions}
+            onChange={(e, data) => setPrefix(data.value)}
+          />
+        </Form.Field>
+
+        <Form.Field>
+        <label> Will the vehicle be used for commercial purposes?</label>
+          <Radio
+            label='Yes'
+            name='radioGroup'
+            value= 'Yes'
+          />
+        </Form.Field>
+        <Form.Field>
+          <Radio
+            label='No'
+            name='radioGroup'
+            value='No'
+          />
+        </Form.Field>
+        <label>Will the vehicle be used outside the registered state?</label>
+        <Form.Field>
+          <Radio
+            label='Yes'
+            name='radioGroup2'
+            value= 'Yes'
+          />
+        </Form.Field>
+        <Form.Field>
+          <Radio
+            label='No'
+            name='radioGroup2'
+            value='No'
+          />
+        </Form.Field>
+        <Form.Field>
+          <label>What is the current value of the vehicle (range 0 - 50000)?</label>
+          <input
+            required
+            placeholder="range 0 - 50000"
+            onChange={(e) => setPhoneNumber(e.target.value)}
+          />
+        </Form.Field>
+
+        
+
+
+
+
+
+
         <Button  required type="submit" onClick={callMockAPI}>
           Submit
         </Button>
