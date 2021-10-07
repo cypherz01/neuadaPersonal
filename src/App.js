d14e6b463ec5867b264ab8cfe314f1e1a05372cc
cypherz01 <mrtomcuthbertson@gmail.com>
2021-10-07 13:42:19 +0000

    added admin page, removed seperate update and delete pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import 'semantic-ui-css/semantic.min.css'
 import "./App.css";
 import Create from "./components/Create/Create";
-import Delete from "./components/Delete/Delete";
 import Read from "./components/Read/Read";
-import Update from "./components/Update/Update";
 import {BrowserRouter, Route} from "react-router-dom";
 
 function App() {
@@ -15,12 +13,6 @@ function App() {
     </div>
     <div>
     <Route exact path ="/read" component={Read}></Route>
-    </div>
-    <div>
-    <Route exact path ="/update" component={Update}></Route>
-    </div>
-    <div>
-    <Route  exact path="/delete" component={Delete}></Route>
     </div>
   </div>
   </BrowserRouter>
--- src/components/Read/Read.js
+++ src/components/Read/Read.js
@@ -1,19 +1,43 @@
 import React, { useState, useEffect } from "react";
-import { Button, Icon, Label, Menu, Table } from "semantic-ui-react";
+import { Button, Form, Checkbox, Table } from "semantic-ui-react";
 import "./Read.css";
 import axios from "axios";
-import { Link } from "react-router-dom";
 
 export default function Read() {
   const [tableData, setTableData] = useState([]);
+  const [inputId, setInputId] = useState("");
+  const [newTelephone, setNewTelephone] = useState("");
+  
+  const userDetails = {
+    telephoneNumber: newTelephone,
+  };
 
-  function callMockApi() {
-    const endPointURL = "https://6151d1954a5f22001701d471.mockapi.io/people";
+  function callMockApi(command) {
+    const endPointURL = `https://6151d1954a5f22001701d471.mockapi.io/people/${inputId}`;
 
-    axios
-      .get(endPointURL)
-      .then((response) => setTableData(response.data))
-      .catch((response) => console.log(response));
+    switch (command) {
+      case "GET":
+        axios
+          .get(endPointURL)
+          .then((response) => setTableData(response.data))
+          .catch((response) => console.log(response));
+        return;
+      case "UPDATE":
+        axios
+          .put(endPointURL, userDetails)
+          .then((response) => setTableData(response.data))
+          .catch((response) => console.log(response));
+        return;
+      case "DELETE":
+        axios
+          .delete(endPointURL)
+          .then((response) => setTableData(response.data))
+          .catch((response) => console.log(response));
+        return;
+      default:
+        console.log("error");
+        return;
+    }
   }
 
   function setLocalStorage(data) {
@@ -21,8 +45,19 @@ export default function Read() {
     localStorage.setItem("prefix", data.prefix);
     localStorage.setItem("firstName", data.firstName);
     localStorage.setItem("lastName", data.lastName);
-    localStorage.setItem("gender", data.gender);
-    localStorage.setItem("phoneNumber", data.phoneNumber);
+    localStorage.setItem("addressLineOne", data.addressLineOne);
+    localStorage.setItem("addressLineTwo", data.addressLineTwo);
+    localStorage.setItem("addressCity", data.addressCity);
+    localStorage.setItem("addressPostCode", data.addressPostCode);
+    localStorage.setItem("vehicleType", data.vehicleType);
+    localStorage.setItem("additionalDrivers", data.additionalDrivers);
+    localStorage.setItem("isCommercial", data.isCommercial);
+    localStorage.setItem(
+      "isRegisteredOutsideState",
+      data.isRegisteredOutsideState
+    );
+    localStorage.setItem("telephoneNumber", data.telephoneNumber);
+    localStorage.setItem("vehicleValue", data.vehicleValue);
   }
 
   useEffect(() => {
@@ -31,9 +66,45 @@ export default function Read() {
 
   return (
     <div>
-      <Link to="/create">
-        <Button color="green">Add New Entry</Button>
-      </Link>
+      <Form>
+        <Form.Field>
+          <label>enter driver ID</label>
+          <input
+            placeholder="ID"
+            onChange={(e) => setInputId(e.target.value)}
+          />
+          <Button color="blue" onClick={() => callMockApi("GET")}>
+            GET
+          </Button>
+        </Form.Field>
+
+        <Form.Field>
+          <label>enter driver ID</label>
+          <input
+            placeholder="ID"
+            onChange={(e) => setInputId(e.target.value)}
+          />
+           <label>Enter New Telephone Number</label>
+          <input
+            placeholder="telephone"
+            onChange={(e) => setNewTelephone(e.target.value)}
+          />
+          <Button color="green" onClick={() => callMockApi("UPDATE")}>
+            UPDATE
+          </Button>
+        </Form.Field>
+
+        <Form.Field>
+          <label>enter driver ID</label>
+          <input
+            placeholder="ID"
+            onChange={(e) => setInputId(e.target.value)}
+          />
+          <Button color="red" onClick={() => callMockApi("DELETE")}>
+            DELETE
+          </Button>
+        </Form.Field>
+      </Form>
       <Table celled>
         <Table.Header>
           <Table.Row>
@@ -41,37 +112,23 @@ export default function Read() {
             <Table.HeaderCell>prefix</Table.HeaderCell>
             <Table.HeaderCell>First Name</Table.HeaderCell>
             <Table.HeaderCell>Last Name</Table.HeaderCell>
-            <Table.HeaderCell>Gender</Table.HeaderCell>
-            <Table.HeaderCell>Phone Number</Table.HeaderCell>
-            <Table.HeaderCell>Update</Table.HeaderCell>
-            <Table.HeaderCell>Delete</Table.HeaderCell>
+            <Table.HeaderCell>Address 1</Table.HeaderCell>
+            <Table.HeaderCell>Adddress 2</Table.HeaderCell>
+            <Table.HeaderCell>City</Table.HeaderCell>
+            <Table.HeaderCell>Postcode</Table.HeaderCell>
           </Table.Row>
         </Table.Header>
         <Table.Body>
-          {tableData.map((data) => (
-            <Table.Row key={data.id}>
-              <Table.Cell>{data.id}</Table.Cell>
-              <Table.Cell>{data.prefix}</Table.Cell>
-              <Table.Cell>{data.firstName}</Table.Cell>
-              <Table.Cell>{data.lastName}</Table.Cell>
-              <Table.Cell>{data.gender}</Table.Cell>
-              <Table.Cell>{data.phoneNumber}</Table.Cell>
-              <Table.Cell>
-                <Link to="/update">
-                  <Button color="green" onClick={() => setLocalStorage(data)}>
-                    Update
-                  </Button>
-                </Link>
-              </Table.Cell>
-              <Table.Cell>
-                <Link to="/delete">
-                  <Button color="red" onClick={() => setLocalStorage(data)}>
-                    Delete
-                  </Button>
-                </Link>
-              </Table.Cell>
+            <Table.Row>
+              <Table.Cell>{tableData.id}</Table.Cell>
+              <Table.Cell>{tableData.prefix}</Table.Cell>
+              <Table.Cell>{tableData.firstName}</Table.Cell>
+              <Table.Cell>{tableData.lastName}</Table.Cell>
+              <Table.Cell>{tableData.addressLineOne}</Table.Cell>
+              <Table.Cell>{tableData.addressLineTwo}</Table.Cell>
+              <Table.Cell>{tableData.addressCity}</Table.Cell>
+              <Table.Cell>{tableData.addressPostCode}</Table.Cell>
             </Table.Row>
-          ))}
         </Table.Body>
       </Table>
     </div>
