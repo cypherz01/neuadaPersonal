61f3160c36f19528434e5b0ec482fc3eedda59ac
cypherz01 <mrtomcuthbertson@gmail.com>
2021-10-18 11:23:11 +0000

    added react hook form and hookform error message for data validation

--- src/components/Create/Create.js
+++ src/components/Create/Create.js
@@ -1,12 +1,26 @@
-import React, { useState} from "react";
+import React, { useState } from "react";
 import "./Create.css";
 import { Button, Form, Dropdown, Radio } from "semantic-ui-react";
 import axios from "axios";
-import {prefixOptions,vehicleTypeOptions, engineSizeOptions, additionalDriversOptions} from "./options.js";
-import SemanticDatepicker from 'react-semantic-ui-datepickers';
-import 'react-semantic-ui-datepickers/dist/react-semantic-ui-datepickers.css';
+import {
+  prefixOptions,
+  vehicleTypeOptions,
+  engineSizeOptions,
+  additionalDriversOptions,
+} from "./options.js";
+import SemanticDatepicker from "react-semantic-ui-datepickers";
+import { useForm } from "react-hook-form";
+import { ErrorMessage } from "@hookform/error-message";
+import "react-semantic-ui-datepickers/dist/react-semantic-ui-datepickers.css";
 
 export default function Create() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    getValues,
+  } = useForm();
+
   const [prefix, setPrefix] = useState("");
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
@@ -22,7 +36,7 @@ export default function Create() {
   const [isRegisteredOutsideState, setIsRegisteredOutsideState] = useState("");
   const [vehicleValue, setVehicleValue] = useState("");
   const [dateRegistered, setDateRegistered] = useState(null);
-  
+
   const endPointURL = "https://6151d1954a5f22001701d471.mockapi.io/people";
 
   const userDetails = {
@@ -40,7 +54,7 @@ export default function Create() {
     isRegisteredOutsideState: isRegisteredOutsideState,
     telephoneNumber: telephoneNumber,
     vehicleValue: vehicleValue,
-    dateRegistered:dateRegistered
+    dateRegistered: dateRegistered,
   };
 
   const callMockAPI = () => {
@@ -52,112 +66,129 @@ export default function Create() {
 
   return (
     <div>
-      <Form>
-        <Form.Group required widths="equal">
-          <Form.Field>
+      <Form onSubmit={handleSubmit(callMockAPI)}>
+        <Form.Group widths="equal">
+          <Form.Field error={!!errors.prefix}>
             <label>Prefix</label>
             <Dropdown
-              required
               placeholder="Prefix"
               fluid
               selection
               options={prefixOptions}
               onChange={(e, data) => setPrefix(data.value)}
+              {...register("prefix", { required: "prefix is mandatory" })}
             />
+            <ErrorMessage errors={errors} name="prefix" />
           </Form.Field>
-          <Form.Input
-            required
-            fluid
-            label="First Name"
-            placeholder="First name"
-            onChange={(e) => setFirstName(e.target.value)}
-          />
-          <Form.Input
-            required
-            fluid
-            label="Last Name"
-            placeholder="Last Name"
-            onChange={(e) => setLastName(e.target.value)}
-          />
+
+          <Form.Field error={!!errors.firstName}>
+            <Form.Input
+              fluid
+              label="First Name"
+              placeholder="First Name"
+              onChange={(e) => setFirstName(e.target.value)}
+              {...register("firstName", {
+                required: "First Name is mandatory",
+              })}
+            />
+            <ErrorMessage errors={errors} name="firstName" />
+          </Form.Field>
+
+          <Form.Field error={!!errors.lastName}>
+            <Form.Input
+              fluid
+              label="Last Name"
+              placeholder="Last Name"
+              onChange={(e) => setLastName(e.target.value)}
+              {...register("lastName", {
+                required: "Last Name is mandatory",
+              })}
+            />
+            <ErrorMessage errors={errors} name="lastName" />
+          </Form.Field>
+
         </Form.Group>
 
-        <Form.Field>
+        <Form.Field error={!!errors.telephoneNumber}>
           <label>telephone Number</label>
           <input
-            required
             placeholder="phoneNumber"
             onChange={(e) => setTelephoneNumber(e.target.value)}
+            {...register("telephoneNumber", {
+              required: "telephone Number is mandatory",
+            })}
           />
+          <ErrorMessage errors={errors} name="telephoneNumber" />
         </Form.Field>
 
         <Form.Group widths="equal">
           <Form.Input
-            required
             fluid
             label="Address Line 1"
             placeholder="Address Line 1"
             onChange={(e) => setAddressLineOne(e.target.value)}
           />
+          <ErrorMessage errors={errors} name="addressLineOne" />
           <Form.Input
-            required
             fluid
             label="Address Line 2"
             placeholder="Address Line 2"
             onChange={(e) => setAddressLineTwo(e.target.value)}
           />
+          <ErrorMessage errors={errors} name="addressLineTwo" />
         </Form.Group>
 
         <Form.Group widths="equal">
           <Form.Input
-            required
             fluid
             label="City"
             placeholder="City"
             onChange={(e) => setAddressCity(e.target.value)}
           />
+          <ErrorMessage errors={errors} name="addressCity" />
           <Form.Input
-            required
             fluid
             label="Postcode"
             placeholder="Postcode"
             onChange={(e) => setAddressPostCode(e.target.value)}
           />
+          <ErrorMessage errors={errors} name="addressPostcode" />
         </Form.Group>
 
         <Form.Field>
           <label>Vehicle Type</label>
           <Dropdown
-            required
             placeholder="Vehicle  Type"
             fluid
             selection
             options={vehicleTypeOptions}
             onChange={(e, data) => setVehicleType(data.value)}
           />
+          <ErrorMessage errors={errors} name="vehicleType" />
         </Form.Field>
 
         <Form.Field>
           <label>Engine Size</label>
           <Dropdown
-            required
             placeholder="Engine Size"
             fluid
             selection
             options={engineSizeOptions}
             onChange={(e, data) => setEngineSize(data.value)}
           />
+          <ErrorMessage errors={errors} name="engineSize" />
         </Form.Field>
 
         <Form.Field>
           <label>Additional Drivers</label>
           <Dropdown
-            required
             placeholder="Additional Drivers"
             fluid
             selection
             options={additionalDriversOptions}
             onChange={(e, data) => setAdditionalDrivers(data.value)}
           />
+          <ErrorMessage errors={errors} name="additionalDrivers" />
         </Form.Field>
 
         <Form.Field>
@@ -195,24 +226,22 @@ export default function Create() {
             What is the current value of the vehicle (range 0 - 50000)?
           </label>
           <input
-            required
             placeholder="range 0 - 50000"
             onChange={(e) => setVehicleValue(e.target.value)}
           />
+          <ErrorMessage errors={errors} name="vehicleValue" />
         </Form.Field>
 
         <Form.Field>
-          <label>
-            What is the date the vehicle was first registered?
-          </label>
+          <label>What is the date the vehicle was first registered?</label>
           <SemanticDatepicker
-            required
             datePickerOnly={true}
-            onChange={(e,data) => setDateRegistered(data.value)}
+            onChange={(e, data) => setDateRegistered(data.value)}
           />
+          <ErrorMessage errors={errors} name="dateRegistered" />
         </Form.Field>
 
-        <Button required type="submit" onClick={callMockAPI}>
+        <Button type="submit" onClick={callMockAPI}>
           Submit
         </Button>
       </Form>
